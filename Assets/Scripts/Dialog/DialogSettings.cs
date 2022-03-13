871e58d151a12639dad5206a0f53ce98170bef1d
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-03-13 16:51:15 -0300

    165112032022

--- Assets/Scripts/Dialog/DialogSettings.cs
+++ Assets/Scripts/Dialog/DialogSettings.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(fileName ="Script Dialogo", menuName ="Criar Dialogo/Novo dialogo")]
 public class DialogSettings : ScriptableObject
@@ -30,3 +31,33 @@ public class Languages
     public string english;
     public string spanish;
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(DialogSettings))]
+public class BuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DialogSettings ds = (DialogSettings)target;
+        Languages linguage = new Languages();
+        linguage.portugues = ds.sentence;
+
+        Sentences sentence = new Sentences();
+        sentence.profile = ds.speakerSprite;
+        sentence.sentence = linguage;
+
+        if (GUILayout.Button("Create_Dialog"))
+        {
+            if (ds.sentence != "") 
+            {
+                ds.dialogs.Add(sentence);
+                ds.speakerSprite = null;
+                ds.sentence = "";
+            } 
+        }
+    }
+}
+
+#endif
